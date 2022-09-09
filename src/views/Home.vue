7232b0ca00830f1b5dd1d46088357ddca000c3f7
Jolonte <vinicius.nevesco@gmail.com>
2022-09-08 21:23:04 -0300

    :sparkles: feat: Main: Benefits & About-prev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <!-- hero -->
+    <!-- header hero -->
     <header class="flex flex-row min-h-screen">
         <!-- titles -->
         <div class="flex flex-col w-6/12 px-28 justify-center gap-3">
@@ -13,14 +13,14 @@
         </div>
 
         <!-- cover -->
-        <div class="w-6/12 bg-[url('src/assets/img.jpg')] bg-no-repeat bg-cover bg-center">
+        <div class="w-6/12 bg-[url('src/assets/hero-img.jpg')] bg-no-repeat bg-cover bg-center">
         </div>
     </header>
 
     <main class="flex flex-col px-28">
 
         <!-- benefits -->
-        <div class="flex flex-col mt-12 gap-12">
+        <section class="flex flex-col mt-12 gap-12">
             <!-- titles -->
             <div class="flex flex-col items-center gap-3">
                 <p class="text-sm text-neutral-500 uppercase">Lorem ipsum dolor sit amet</p>
@@ -38,7 +38,6 @@
                         <hr class="w-1/3 border-2 border-solid border-green-400">
                     </div>
                 </div>
-
                 <div class="flex flex-col w-full h-96 shadow-md">
                     <div class="bg-neutral-800 h-3/6"></div>
                     <div class="flex flex-col px-4 h-3/6 items-center justify-evenly gap-2">
@@ -47,7 +46,6 @@
                         <hr class="w-1/3 border-2 border-solid border-green-400">
                     </div>
                 </div>
-
                 <div class="flex flex-col w-full h-96 shadow-md">
                     <div class="bg-neutral-800 h-3/6"></div>
                     <div class="flex flex-col px-4 h-3/6 items-center justify-evenly gap-2">
@@ -57,7 +55,27 @@
                     </div>
                 </div>
             </div>
-        </div>
-        
+        </section>
+
+        <!-- about prev -->
+        <section class="relative h-128 mt-12">
+            <!-- img -->
+            <div class="static w-7/12 h-full bg-[url('src/assets/about-prev.jpg')] bg-no-repeat bg-cover bg-center">
+
+                <!-- card cover -->
+                <div class="m-auto absolute right-0 flex w-3/6 h-full items-center">
+                    <!-- card -->
+                    <div class="flex flex-col p-10 h-fit gap-4 bg-white">
+                        <p class="text-sm text-neutral-500 uppercase">Lorem ipsum dolor sit amet</p>
+                        <h3 class="text-3xl font-medium font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                        <p class="text-neutral-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Metus aliquam eleifend mi in. Eu facilisis sed odio morbi quis commodo odio aenean. Eget mauris pharetra et ultrices neque ornare.</p>
+                        <button class="w-56 h-12 font-medium uppercase text-sm text-white bg-neutral-800">Sobre nós</button>
+                    </div>
+                </div>
+            </div>
+
+            
+
+        </section>
     </main>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+      }
+    },
   },
   plugins: [],
 }
