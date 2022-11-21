675ce62c06b6046d0b9571c3c3475bf79f0f8172
Jolonte <vinicius.nevesco@gmail.com>
2022-11-20 21:18:22 -0300

    :lipstick: feat: Font & Colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    colors: {
+      primary: '#6C584C',
+      secondary: '#A98467',
+      tertinary: '#ADC178',
+      green1: '#DDE5B6',
+      green2: '#F7F4E8'
+    }
   },
   plugins: []
 }
