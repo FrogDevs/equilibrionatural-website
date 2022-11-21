b106e548e5470a373fe8109ead5a1346b29744f5
Jolonte <vinicius.nevesco@gmail.com>
2022-11-20 21:16:21 -0300

    :sparkles: feat: Products route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: () => import('../views/TheHome.vue') },
-    { path: '/sobre', component: () => import('../views/TheAbout.vue') },
+    { path: '/sobre', component: () => import('../views/TheProducts.vue') },
     { path: '/contato', component: () => import('../views/TheContact.vue') },
     {
       path: '/:catchAll(.*)',
