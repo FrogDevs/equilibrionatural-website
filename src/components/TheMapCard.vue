2b8214cfb6736a2c038588e8285d2e1ec4217811
Jolonte <vinicius.nevesco@gmail.com>
2022-11-23 18:51:14 -0300

    :sparkles: feat: Google Maps
    Google Maps embed in iframe tag

--- src/components/TheMapCard.vue
+++ src/components/TheMapCard.vue
@@ -3,6 +3,10 @@ const props = defineProps({
   title: {
     type: String,
     default: ''
+  },
+  mapSource: {
+    type: String,
+    default: ''
   }
 })
 </script>
@@ -11,8 +15,13 @@ const props = defineProps({
     <p class="text-center text-[1.25rem] leading-[1.875rem] text-secondary">
       {{ props.title }}
     </p>
-    <div
+    <iframe
       class="h-[10rem] w-[16.875rem] rounded-[1.25rem] bg-green2 md:h-[15rem] md:w-full 2xl:h-[20rem]"
-    ></div>
+      :src="props.mapSource"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+    ></iframe>
   </div>
 </template>
