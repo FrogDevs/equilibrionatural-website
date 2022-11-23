f4c84bb4f53e76df71851f1f60b9a8d4c4f47c14
Jolonte <vinicius.nevesco@gmail.com>
2022-11-23 18:51:35 -0300

    :sparkles: feat: 404 View

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -1,4 +1,25 @@
 <script setup></script>
 <template>
-  <p>404 error</p>
+  <main
+    class="flex h-screen flex-col items-center justify-center gap-[1.25rem] p-[3.125rem] lg:gap-[2.5rem]"
+  >
+    <h1
+      class="text-center text-[1.563rem] font-bold leading-[1.875rem] text-primary lg:text-[3.125rem] lg:leading-[3.688rem]"
+    >
+      Oops! Página não encontrada
+    </h1>
+    <router-link
+      to="/"
+      class="cursor-pointer text-center text-[1.25rem] leading-[1.875rem] text-secondary duration-200 ease-out hover:text-[#8F6F57] lg:text-[1.563rem]"
+    >
+      🪴 Voltar a página inicial 🍃
+    </router-link>
+    <img
+      class="pb-[0.5rem] md:w-1/3"
+      src="/art5.svg"
+      alt="Relaxando com o equilíbrio natural"
+      draggable="false"
+      loading="lazy"
+    />
+  </main>
 </template>
