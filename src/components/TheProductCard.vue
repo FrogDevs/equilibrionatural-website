63182d0260d149f49017da50289da51406ee7f28
Jolonte <vinicius.nevesco@gmail.com>
2022-11-26 10:28:05 -0300

    :lipstick: fix: Desing fixes

--- src/components/TheProductCard.vue
+++ src/components/TheProductCard.vue
@@ -4,10 +4,6 @@ const props = defineProps({
     type: String,
     default: ''
   },
-  subTitle: {
-    type: String,
-    default: ''
-  },
   img: {
     type: String,
     default: ''
@@ -15,7 +11,9 @@ const props = defineProps({
 })
 </script>
 <template>
-  <div class="h-[16.875rem] w-[16.875rem] rounded-[1.25rem] bg-green2">
+  <div
+    class="h-[16.875rem] w-[16.875rem] cursor-pointer rounded-[1.25rem] bg-green2 transition-colors duration-200 ease-out hover:bg-[#f0ead2]"
+  >
     <img
       class="h-[60%] w-full rounded-[1.25rem] bg-tertinary object-cover"
       :src="props.img"
